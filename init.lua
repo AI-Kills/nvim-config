c137e82731bb9c8b50f58bb438b18a41a148c495
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-04-26 10:39:18 +0200

    add mapping of c + option to $

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- Then load the rest of the configuration
 require("config.lazy")
+
+vim.g.snacks_animate = false
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- open the command line in normal mode with leader + enter
 vim.keymap.set("n", "<leader><CR>", ":", { noremap = true, silent = false, desc = "Open command line" })
---
+
 -- save all files with leader  sf
 vim.keymap.set("n", "<leader>sf", ":wa<CR>", { noremap = true, silent = true })
 
