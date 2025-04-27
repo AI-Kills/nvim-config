449ff47732b36876d071082683f49df7605f9118
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-04-27 09:26:18 +0200

    let telescope show hidden files with <leader>ff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,10 @@ vim.keymap.set("n", "<leader><leader>", ":", { noremap = true, silent = false, d
 vim.keymap.set("n", "<leader>dm", ":delmarks a-z<CR>", { desc = "Delete all local marks" })
 
 vim.keymap.set("n", "<leader>k", "'", { noremap = true, silent = true, desc = "Go to mark" })
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>ff",
+  ':lua require"telescope.builtin".find_files({ hidden = true })<CR>',
+  { noremap = true, silent = true }
+)
