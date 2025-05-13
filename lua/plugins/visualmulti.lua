864beacf7e1f78a84048c53d5b0cafb13d0433ca
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-05-13 21:32:35 +0200

    feat: add new cursor up mapping and improve visual multi plugin configuration

--- lua/plugins/visualmulti.lua
+++ lua/plugins/visualmulti.lua
@@ -1,18 +1,24 @@
 return {
-  "mg979/vim-visual-multi",
-  init = function()
-    vim.g.VM_default_mappings = 0 -- disabilita i mappings di default
+    "mg979/vim-visual-multi",
+    init = function()
+        vim.g.VM_default_mappings = 0 -- disabilita i mappings di default
 
-    -- Definisci i tuoi mapping personalizzati sotto <leader>m
-    -- === NORMAL MODE ===
-    vim.keymap.set("n", "<leader>mn", "<Plug>(VM-Fnd-Under)", { desc = "Multi: Seleziona parola" })
-    vim.keymap.set("n", "<leader>ma", "<Plug>(VM-Select-All)", { desc = "Multi: Seleziona tutte le occorrenze" })
-    vim.keymap.set("n", "<leader>mj", "<Plug>(VM-Find-Next)", { desc = "Multi: Prossima occorrenza" })
-    vim.keymap.set("n", "<leader>j", "<Plug>(VM-Add-Cursor-Down)", { desc = "Multi: Cursore giù" })
-    vim.keymap.set("n", "<leader>mk", "<Plug>(VM-Find-Prev)", { desc = "Multi: Occorrenza precedente" })
-    vim.keymap.set("n", "<leader>md", "<Plug>(VM-Remove-Last-Cursor)", { desc = "Multi: Rimuovi ultimo cursore" })
+        -- Definisci i tuoi mapping personalizzati sotto <leader>m
+        -- === NORMAL MODE ===
+        vim.keymap.set("n", "<leader>mn", "<Plug>(VM-Fnd-Under)", { desc = "Multi: Seleziona parola" })
+        vim.keymap.set("n", "<leader>ma", "<Plug>(VM-Select-All)", { desc = "Multi: Seleziona tutte le occorrenze" })
+        vim.keymap.set("n", "<leader>mj", "<Plug>(VM-Find-Next)", { desc = "Multi: Prossima occorrenza" })
+        vim.keymap.set("n", "<leader>j", "<Plug>(VM-Add-Cursor-Down)", { desc = "Multi: Cursore giù" })
+        vim.keymap.set("n", "<leader>k", "<Plug>(VM-Add-Cursor-Up)", { desc = "Multi: Cursore giù" })
+        vim.keymap.set("n", "<leader>mk", "<Plug>(VM-Find-Prev)", { desc = "Multi: Occorrenza precedente" })
+        vim.keymap.set("n", "<leader>md", "<Plug>(VM-Remove-Last-Cursor)", { desc = "Multi: Rimuovi ultimo cursore" })
 
-    -- === VISUAL MODE ===
-    vim.keymap.set("v", "<leader>ma", "<Plug>(VM-Visual-All)", { desc = "Multi: Tutte le occorrenze del selezionato" })
-  end,
+        -- === VISUAL MODE ===
+        vim.keymap.set(
+            "v",
+            "<leader>ma",
+            "<Plug>(VM-Visual-All)",
+            { desc = "Multi: Tutte le occorrenze del selezionato" }
+        )
+    end,
 }
