4e8138508c6bd92031f51d22af5bcaeff6283562
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-05-13 22:12:49 +0200

    refactor: optimize keymaps and add smart percent navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,3 @@
--- custom commandline
-vim.keymap.set("n", ";", ":", {
-    noremap = true,
-    silent = true,
-    desc = "Command line",
-})
 -- Save all
 vim.keymap.set("n", "<leader>sa", ":wa<CR>", {
     noremap = true,
@@ -24,7 +18,6 @@ vim.api.nvim_set_keymap(
 )
 
 -- move among buffers
-vim.keymap.set("n", ".", ":bnext<CR>", { noremap = true, silent = true, desc = "go to next buffer" })
 vim.keymap.set("n", ",", ":bprevious<CR>", { noremap = true, silent = true, desc = "go to previous buffer" })
 
 -- create splits
@@ -49,4 +42,35 @@ vim.keymap.set("n", "h", "za", {
     noremap = true,
     silent = true,
     desc = "open fold",
+})
+
+-- Funzione “smart %” ottimizzata:
+-- 1) se sei su () [] {} → salto standard
+-- 2) altrimenti cerca all’indietro il primo tra ( [ {
+local function smart_percent()
+    -- 1) carattere sotto il cursore
+    local col = vim.fn.col(".")
+    local line = vim.fn.getline(".")
+    local char = line:sub(col, col)
+
+    -- 2) se è una parentesi, salto standard
+    if char:match("[%(%%)%[%]%{%}]") then
+        return "%"
+    end
+
+    -- 3) altrimenti cerca backwards il primo tra ( [ {
+    --    '[([{]' = classe contenente '(', '[' o '{'
+    --    'bW' = backwards, no wrap-around
+    vim.fn.search("[([{]", "bW")
+
+    -- 4) ritorno vuoto perché la ricerca ha già spostato il cursore
+    return ""
+end
+
+-- Mapping “expr”: valuta smart_percent() e ne usa il risultato
+vim.keymap.set("n", "%", smart_percent, {
+    expr = true,
+    noremap = true,
+    silent = true,
+    desc = "Smart %: match standard oppure previous tra '(', '[', '{'",
 })
