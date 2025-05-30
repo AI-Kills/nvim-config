9436e1b86e6d57a38a7a67ed3186237da1178ca5
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-05-30 12:43:08 +0200

    add transparency to the colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -18,7 +18,7 @@ return {
             "aliqyan-21/darkvoid.nvim",
             priority = 1000,
             opts = {
-                transparent = false,
+                transparent = true,
                 glow = true,
                 show_end_of_buffer = true,
 
